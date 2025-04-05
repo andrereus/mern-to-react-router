fc7d81f71f5728f9a80c035167b89e620ee3ba57
Andre Reus <andrereus@users.noreply.github.com>
2025-04-05 23:03:55 +0200

    Mutations, Creating, Updating, Redirecting

--- react/app/routes.ts
+++ react/app/routes.ts
@@ -5,6 +5,7 @@ export default [
   layout("layouts/sidebar.tsx", [
     index("routes/home.tsx"),
     route("contacts/:contactId", "routes/contact.tsx"),
+    route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
     route("employees", "routes/record-list.tsx", { id: "record-list" }),
     route("employees/edit/:id", "routes/record.tsx", { id: "record-edit" }),
     route("employees/create", "routes/record.tsx", { id: "record-create" }),
