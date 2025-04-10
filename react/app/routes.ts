b9dd645c89265c7c93150f2dda43ff8057435fe1
Andre Reus <andrereus@users.noreply.github.com>
2025-04-10 20:26:49 +0200

    Delete & Cancel

--- react/app/routes.ts
+++ react/app/routes.ts
@@ -6,6 +6,7 @@ export default [
     index("routes/home.tsx"),
     route("contacts/:contactId", "routes/contact.tsx"),
     route("contacts/:contactId/edit", "routes/edit-contact.tsx"),
+    route("contacts/:contactId/destroy", "routes/destroy-contact.tsx"),
     route("employees", "routes/record-list.tsx", { id: "record-list" }),
     route("employees/edit/:id", "routes/record.tsx", { id: "record-edit" }),
     route("employees/create", "routes/record.tsx", { id: "record-create" }),
